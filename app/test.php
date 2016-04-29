a9b6c3617305ba8a2851ec966dce0ae23ed18867
tarunjoshi21 <tarunjoshi21@gmail.com>
2016-04-29 19:15:32 +0530

    php file

---
+++ app/test.php
@@ -0,0 +1,61 @@
+<?php
+// Check if image file is a actual image or fake image
+if(isset($_FILES["profilePicture"]["tmp_name"])) {
+
+    $target_dir = "/opt/lampp/htdocs/bluto_uploads/";
+$target_file = $target_dir . basename($_FILES["profilePicture"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+
+
+    $check = getimagesize($_FILES["profilePicture"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["profilePicture"]["size"] > 5000000000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["profilePicture"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["profilePicture"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+}
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="test.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="profilePicture" id="profilePicture">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
